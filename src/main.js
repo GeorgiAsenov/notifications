87190b872dba373a32315c726e07d787fd5769b8
Georgi Asenov <gasenov@weband.bg>
2019-11-06 22:56:13 +0200

    feat: adding dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import Api from '@/services/ApiService'
+import VShowSlide from 'v-show-slide'
 
+Vue.prototype.$api = Api
 Vue.config.productionTip = false;
+
+Vue.use(VShowSlide)
 
 new Vue({
   store,
